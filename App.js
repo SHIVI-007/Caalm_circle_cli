7d4eeb05da14d9de5ae8835bf49161fa97f98644
SHIVI-007 <shivansgupta200@gmail.com>
2023-03-05 23:45:07 +0530

    Circle_ci commit

---
+++ App.js
@@ -0,0 +1,53 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import { AuthProvider } from './src/context/AuthContext';
+import RouteStack from './src/route/RouteStack';
+
+
+
+function App() {
+  
+
+  return (
+    <>
+      <AuthProvider>
+        <RouteStack />
+      </AuthProvider>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
